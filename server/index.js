783c39ca91ea85f7a68ea127ccef8ca37ee0f3f3
Zaina Rodney <zainarodney@gmail.com>
2020-09-02 00:15:42 -0400

    added error handling

--- server/index.js
+++ server/index.js
@@ -8,18 +8,24 @@ app.use(morgan('dev'))
 
 app.use(express.json())
 
-//use express.static() MAKE SURE THE PATH TO YOUR PUBLIC FOLDER IS RIGHT!
+
 app.use(express.static(path.join(__dirname,'/public')))
 
 app.get('/', (req, res, next) => {
   res.render('index.html')
 })
 
-//require in your routes and use them on your api path
 app.use('/api', require('./routes/apiRoutes'))
-//404 handler
 
-//500 handler
+
+app.use((req, res, next) => {
+  res.status(404).send(`Uh-Oh, page not found`)
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message)
+})
+
 
 
 const port = process.env.PORT || 3035;
@@ -31,5 +37,3 @@ const init = async () => {
 }
 
 init();
-
-//listen
--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router()
-const { db } = require('../db/seed')
-//import models from /db
-
-
-router.use('/api', require('./apiRoutes'))
-
-module.exports = router
