70130629c4d0b5f4e4af5abb932cae22cb9945c9
Zaina Rodney <zainarodney@gmail.com>
2020-09-02 00:02:19 -0400

    finished detail view and set up handling for hashchanges

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -10,6 +10,17 @@ router.get('/plants', async (req, res, next) => {
   }
 })
 
+router.get('/plants/:id', async (req, res, next) => {
+  try {
+    const plant = await Plant.findByPk(req.params.id, {
+      include: Detail
+    })
+    res.send(plant)
+  } catch(err) {
+    next(err)
+  }
+})
+
 router.get('/details', async (req, res, next) => {
   try {
     const details = await Detail.findAll();
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,7 @@
 const router = require("express").Router()
 const { db } = require('../db/seed')
-//const apiRoutes = require('./apiRoutes')
 //import models from /db
 
-//routes go here
 
 router.use('/api', require('./apiRoutes'))
 
